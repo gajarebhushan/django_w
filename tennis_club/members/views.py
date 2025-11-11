884853f4cfcc52b0db230504a116bdae5dc2b3cb
gajarebhushan34@gmail.com <gajarebhushan34@gmail.com>
2025-11-11 21:42:12 +0530

    restore

--- tennis_club/members/views.py
+++ tennis_club/members/views.py
@@ -1,6 +1,17 @@
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
+'''from django.http import HttpResponse
 
 def members(request):
     return HttpResponse("Hello world!")
+'''
+# Modify the View
+'''from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
+'''
+def members(request):
+    return render(request, 'members/myfirst.html')
